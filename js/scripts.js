1b06b85972bd499910b1e864c83c163425eb1fd2
c-mendes <marques.catarina@gmail.com>
2022-09-22 15:34:10 +0100

    updates after user testing

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,29 @@ function menuToggle() {
     x.className = 'navtoggle';
   }
 }
+
+function openSection(evt, Sections) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(Sections).style.display = "block";
+  evt.currentTarget.className += " active";
+  
+}
+
+  
+
+
